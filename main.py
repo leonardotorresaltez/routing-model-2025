32ce315560c3be094327dc4b8a8176ddfd65c236
clara.gregori <clara.gregori@bonarea.com>
2026-01-26 16:37:16 +0100

    bug corrected in output logs

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ from core.utils.data_loader import MDVRPDataLoader
 
 
 def train():
+    """
+    Reset: Start a fresh day with all customers unvisited.
+    Act: The Agent builds a full plan for the day.
+    Step: The Environment calculates how long that plan took.
+    Learn: Every 10 episodes, the Agent looks back at what worked and updates the Policy's weights via loss.backward().
+    Repeat: This continues for hundreds of episodes until the Agent learns the spatial patterns of the customers.
+    """
     cfg = parse_args()
     os.makedirs("checkpoints", exist_ok=True)
     
@@ -24,7 +31,9 @@ def train():
 
     env = MDVRPEnv(cfg, data)
     agent = MDVRPREINFORCEAgent(cfg, data)
+    batch_rewards = []
 
+    print(f"--> STARTING RUN: {cfg.run_name}")
     pbar = tqdm(range(cfg.episodes))
     for episode in pbar:
         state, _ = env.reset()
@@ -34,12 +43,27 @@ def train():
         state, reward, _, _, info = env.step(action)
         
         agent.store_reward(reward)
-        loss = agent.update()
-        
-        if cfg.wandb:
-            wandb.log({"reward": reward, "loss": loss, "total_time": info["total_time"]})
+        batch_rewards.append(reward) # Track rewards for this batch
+    
+        if (episode + 1) % 10 == 0:
+            loss = agent.update()
+            avg_reward = sum(batch_rewards) / len(batch_rewards)
+            pbar.write(
+                    f"Episode {episode+1:>4} | "
+                    f"Avg Reward: {avg_reward:.3f} | "
+                    f"Loss: {loss:.4f} | "
+                    f"Time: {info['total_time']:.2f}h"
+                )
             
-        pbar.set_description(f"Time: {info['total_time']:.2f}h")
+            if cfg.wandb:
+                wandb.log({
+                    "episode ":episode,
+                    "batch_avg_reward ": avg_reward, 
+                    "loss ": loss, 
+                    "total_time ": info["total_time"]
+                })
+            batch_rewards = [] # Reset for next batch
+            
 
     torch.save(agent.policy.state_dict(), f"checkpoints/mdvrp_{cfg.run_name}.pt")
     if cfg.wandb: wandb.finish()
