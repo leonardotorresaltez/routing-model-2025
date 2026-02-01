a2342388c0584c9d7fda5f29c81e9a439d693a8e
alexortiz1998 <alexortiz1998@gmail.com>
2026-02-01 02:28:14 +0100

    solve bug by which the trucks only started from the depot on the first episode

--- core/envs/tsp_env.py
+++ core/envs/tsp_env.py
@@ -9,18 +9,19 @@ from gymnasium import spaces
 class TSPEnv(gym.Env):
     metadata = {"render_modes": []}
 
-    def __init__(self, cfg,nodes, current_positions, time_matrix):
+    def __init__(self, cfg,nodes, truck_starts, time_matrix):
         super().__init__()
         self.num_nodes = cfg.num_nodes
         self.nodes = nodes
-        self.current_positions = current_positions
+        self.truck_starts = truck_starts
+        self.current_positions = truck_starts.copy()
         self.time_matrix = time_matrix # Store the actual travel times
         
         # ---- Observation space ----
         self.observation_space = spaces.Dict({
         "nodes": spaces.Box(low=0.0, high=1.0, shape=(self.num_nodes, 2), dtype=np.float32),
         "visited": spaces.Box(low=0, high=1, shape=(self.num_nodes,), dtype=np.int8),
-        "current": spaces.MultiDiscrete([self.num_nodes] * len(current_positions))
+        "current": spaces.MultiDiscrete([self.num_nodes] * len(truck_starts))
         })        
         # ---- Action space ----
         self.action_space = spaces.Discrete(self.num_nodes)
@@ -31,12 +32,13 @@ class TSPEnv(gym.Env):
         super().reset(seed=seed)
 
         self.visited = torch.zeros(self.num_nodes, dtype=torch.bool)
+        self.current_positions = self.truck_starts.copy()
         
         # Mark all depots as visited for all trucks
-        for pos in self.current_positions:
+        for pos in self.truck_starts:
             self.visited[pos] = True
         
-        self.tours = [[pos] for pos in self.current_positions]
+        self.tours = [[pos] for pos in self.truck_starts]
         return self._get_obs(), {}
 
     def _get_obs(self):
